d2afb3af32414aacab6ed0c92ae96e3bf2739739
RastyDen <70960711+RastyDen@users.noreply.github.com>
2021-01-27 11:54:27 +0300

    Update ind3.py

--- ind3.py
+++ ind3.py
@@ -3,8 +3,9 @@
 
 import math
 
-for i in range(6):
-    if not (100 - 19 * i) % 9 :
-        print('быков =', i)
-        print('коров =', (100 - 19 * i) // 9)
-        print('телят =', (100 - 10 * i - 5 * (100 - 19 * i) // 9) * 2)
+if __name__ == '__main__':
+    for i in range(6):
+        if not (100 - 19 * i) % 9 :
+            print('быков =', i)
+            print('коров =', (100 - 19 * i) // 9)
+            print('телят =', (100 - 10 * i - 5 * (100 - 19 * i) // 9) * 2)
