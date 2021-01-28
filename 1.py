35556b6780dce36e2d1ac7cde544356576dd9058
RastyDen <70960711+RastyDen@users.noreply.github.com>
2021-01-28 13:32:23 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,10 +9,8 @@ if __name__ == '__main__':
     
     if x <= 0:
         y = 2 * x * x + math.cos(x)
-   
     elif x < 5:
         y = x + 1
-    
     else:
         y = math.sin(x) - x * x
     
