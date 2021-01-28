48e03a31a98d948a5041c507b31d9c1aef1dbafe
RastyDen <70960711+RastyDen@users.noreply.github.com>
2021-01-28 13:33:26 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import sys
+
 if __name__ == '__main__':
     n = int(input("Введите номер месяца: "))
+    
     if n == 1 or n == 2 or n == 12:
         print("Зима")
     elif n == 3 or n == 4 or n == 5:
